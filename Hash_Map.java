39bc1358f334d8c7b0553092888c22fc22d77a1a
adisorn <110546558+adisornsriphongthong@users.noreply.github.com>
2023-07-03 16:23:30 +0700

    Add files via upload

---
+++ Hash_Map.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+public class Hash_Map {
+    public static void main(String[] args) {
+        HashMap<Integer, String> map = new HashMap<>();
+        map.put(1, "pizza");
+        map.put(2, "water");
+        map.put(3, "apple");
+
+        System.out.println(map.getValue);
+
+    }
+}
