86f7ae37cf696ebc383936f6ebf2b867586d937f
adisorn <110546558+adisornsriphongthong@users.noreply.github.com>
2023-07-03 16:18:37 +0700

    Abstract

---
+++ Abstract.java
@@ -0,0 +1,38 @@
+abstract class Abstract{
+    void display(){
+        System.out.println("hello world");
+    }
+
+}
+
+class AbstractTest extends Abstract{
+    public static void main(String[] args) {
+        AbstractTest a = new AbstractTest();
+        a.display();
+    }
+}
+/*
+
+encapsulation
+abstract
+inheritance
+
+modifier
+behavior
+field
+variable
+instance variable
+static
+void
+return
+this
+super
+extends
+method
+object
+overrideing
+overounding
+
+*/
+
+
